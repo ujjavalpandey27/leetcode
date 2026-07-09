1c41ff72d62091afb178bdb3512bda7ce2b50231
ujjavalpandey27 <ujjavalpandeydtucse@gmail.com>
2026-07-09 22:57:22 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 44 MB (87.78%)

---
+++ my-code/0162-find-peak-element/solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+    if(nums.length==1){ 
+        return 0;
+    }
+    if(nums[0]>nums[1]){
+        return 0;
+    }
+    if(nums[nums.length-1]>nums[nums.length-2]){
+        return nums.length-1;
+    }
+
+    int low=1;
+    int high=nums.length-1;
+    while(low<=high){
+        int mid=low+(high-low)/2;
+        if(nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1]){
+            return mid;
+        }
+        else if(nums[mid+1]>nums[mid]){
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
+        }
+    }
+    return -1;
+    }
+}
