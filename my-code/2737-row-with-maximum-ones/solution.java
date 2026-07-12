5e56c531bbd438c18bb2465668cccb76ccd04ea3
ujjavalpandey27 <ujjavalpandeydtucse@gmail.com>
2026-07-12 13:09:08 +0000

    Sync LeetCode submission Runtime - 4 ms (82.23%), Memory - 48.1 MB (89.93%)

---
+++ my-code/2737-row-with-maximum-ones/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] rowAndMaximumOnes(int[][] mat) {
+        int ans=0;
+        int index=0;
+       for(int i=0;i<mat.length;i++){
+        int count=0;
+            for(int j=0;j<mat[i].length;j++){
+                if(mat[i][j]==1){
+                    count++;
+                    
+                }
+            }
+            if(count>ans){
+                        ans=count;
+                        index=i;
+                    }
+       }
+       return new int[]{index,ans};
+    }
+}
